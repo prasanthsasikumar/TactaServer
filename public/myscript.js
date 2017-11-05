bb4b30bd7370b8f14425254af40525a787751330
Prasanth Sasikumar <psa59@canterbury.ac.nz>
2017-11-05 15:33:50 +1300

    clean up

--- public/myscript.js
+++
@@ -1,8 +0,0 @@
-
-
-/*document.getElementById("portsAvail").addEventListener("click", function(e) {
-  if (e.target && e.target.nodeName == "LI") {
-    console.log(e.target.innerText + " was clicked");
-    socket.emit('query', e.target.innerText);
-  }
-});*/
